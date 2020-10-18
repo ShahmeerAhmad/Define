2815c40df7b809edfa157b42cc95dd6554880206
ShahmeerAhmad <shahmeerhafiz@gmail.com>
2020-10-18 20:44:43 +0500

    Change the colors parameter to List gradient

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -5,123 +5,116 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:dictionary/newclass.dart';
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  String _url="https://owlbot.info/api/v4/dictionary/";
-  String _token="30263faa5ef0fdd31dd87f6c3b150dd671605553";
-  TextEditingController _controller=TextEditingController();
+  String _url = "https://owlbot.info/api/v4/dictionary/";
+  String _token = "30263faa5ef0fdd31dd87f6c3b150dd671605553";
+  TextEditingController _controller = TextEditingController();
   StreamController _streamController;
   Stream _stream;
-  bool _status=true;
+  bool _status = true;
   var _connectionStatus = "Enter the search word";
-  var _connectivity ;
+  var _connectivity;
   StreamSubscription<ConnectivityResult> _connectivitySubscription;
-
-
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    _connectivity=Connectivity();
+    _connectivity = Connectivity();
     _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen((ConnectivityResult result){
-          setState(() {
-            if(result==ConnectivityResult.wifi){
-              _connectionStatus="Enter the search word";
-            }else
-            if(result==ConnectivityResult.mobile){
-              _connectionStatus="Enter the search word";
-            }else
-            if(result==ConnectivityResult.none){
-              _connectionStatus=result.toString();
-            }
-          });
-        });
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      setState(() {
+        if (result == ConnectivityResult.wifi) {
+          _connectionStatus = "Enter the search word";
+        } else if (result == ConnectivityResult.mobile) {
+          _connectionStatus = "Enter the search word";
+        } else if (result == ConnectivityResult.none) {
+          _connectionStatus = result.toString();
+        }
+      });
+    });
 
-    _streamController=StreamController();
-    _stream=_streamController.stream;
+    _streamController = StreamController();
+    _stream = _streamController.stream;
   }
-
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
-    if(state==AppLifecycleState.resumed){
+    if (state == AppLifecycleState.resumed) {
       Navigator.of(context).pop();
-      Navigator.push(context, MaterialPageRoute(
-        builder: (context){
-
-          return MyApp();
-        }
-      ));
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return MyApp();
+      }));
     }
   }
 
   Timer _timer;
-  _search() async{
-    if(_controller.text==null || _controller.text.length==0){
+  _search() async {
+    if (_controller.text == null || _controller.text.length == 0) {
       _streamController.add(null);
       return;
     }
     _streamController.add("waiting");
 
-    Response response= await get(_url+_controller.text.trim(),headers: {"Authorization": "Token "+_token});
+    Response response = await get(_url + _controller.text.trim(),
+        headers: {"Authorization": "Token " + _token});
     if (response.statusCode == 200) {
       var jsonResponse = jsonDecode(response.body);
       _streamController.add(jsonResponse);
-    } else if(response.statusCode==404){
+    } else if (response.statusCode == 404) {
       setState(() {
-        _status=false;
+        _status = false;
       });
     }
-
   }
-
 
   @override
   void dispose() {
-
     _connectivitySubscription.cancel();
     super.dispose();
     _streamController.close();
     WidgetsBinding.instance.removeObserver(this);
   }
 
-  showExit(){
+  showExit() {
     return showDialog(
-      context: context,
-      builder: (context){
-        return AlertDialog(
-          title: Text("Confirm",style: TextStyle(color: Colors.pink),),
-          content: Text("Do you want to Exit"),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("No"),
-              onPressed: (){
-                  Navigator.pop(context);
-              },
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              "Confirm",
+              style: TextStyle(color: Colors.pink),
             ),
-            FlatButton(
-              child: Text("Yes"),
-              onPressed: (){
+            content: Text("Do you want to Exit"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("No"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                child: Text("Yes"),
+                onPressed: () {
                   SystemNavigator.pop();
-              },
-            )
-          ],
-        );
-      }
-    );
+                },
+              )
+            ],
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-      onWillPop: (){
+      onWillPop: () {
         return showExit();
       },
       child: Scaffold(
@@ -134,32 +127,34 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
               children: <Widget>[
                 Expanded(
                   child: Container(
-                    margin:EdgeInsets.only(left: 12.0,bottom: 12.0),
+                    margin: EdgeInsets.only(left: 12.0, bottom: 12.0),
                     decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: BorderRadius.circular(24.0)
-                    ),
+                        borderRadius: BorderRadius.circular(24.0)),
                     child: TextFormField(
                       controller: _controller,
                       decoration: InputDecoration(
                           contentPadding: const EdgeInsets.only(left: 28.0),
                           hintText: "Enter the word",
-                          border: InputBorder.none
-                      ),
-                      onChanged: (val){
-                        if(_timer ?.isActive ?? false)_timer.cancel();
-                        _timer= Timer(const Duration(seconds: 1), (){
-                          _status=true;
+                          border: InputBorder.none),
+                      onChanged: (val) {
+                        if (_timer?.isActive ?? false) _timer.cancel();
+                        _timer = Timer(const Duration(seconds: 1), () {
+                          _status = true;
                           _search();
                         });
                       },
                     ),
                   ),
                 ),
-                IconButton(icon: Icon(Icons.search,size: 24,color: Colors.white,),
-                  onPressed: ()=>_search(),
+                IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    size: 24,
+                    color: Colors.white,
+                  ),
+                  onPressed: () => _search(),
                 ),
-
               ],
             ),
           ),
@@ -168,26 +163,32 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
           margin: EdgeInsets.all(8.0),
           child: StreamBuilder(
             stream: _stream,
-            builder: (BuildContext context,AsyncSnapshot snapshot){
-              if(snapshot.data==null){
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.data == null) {
                 return Container(
                   child: Center(
                     child: Text(_connectionStatus),
                   ),
                 );
               }
-              if(snapshot.data=="waiting"){
+              if (snapshot.data == "waiting") {
                 return Center(
-                  child:_status==false?Center(child: Text("Not Found")): CircularProgressIndicator(),
+                  child: _status == false
+                      ? Center(child: Text("Not Found"))
+                      : CircularProgressIndicator(),
                 );
               }
-              var val=_controller.text.toString();
+              var val = _controller.text.toString();
               return ListView.builder(
-                itemCount: snapshot.data["definitions"].length==null?0:snapshot.data["definitions"].length,
-                itemBuilder: (BuildContext context,dynamic index){
+                itemCount: snapshot.data["definitions"].length == null
+                    ? 0
+                    : snapshot.data["definitions"].length,
+                itemBuilder: (BuildContext context, dynamic index) {
                   return ListBody(
                     children: <Widget>[
-                      SizedBox(height: 2,),
+                      SizedBox(
+                        height: 2,
+                      ),
                       Card(
                         elevation: 10,
                         color: Colors.pinkAccent,
@@ -197,40 +198,65 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                               color: Colors.pinkAccent[100],
                               padding: EdgeInsets.all(8.0),
                               child: ListTile(
-
-                                leading: snapshot.data["definitions"][index]["image_url"]==null?null:Hero(
-                                  tag: "$val",
-                                  child: Container(
-                                      child: GestureDetector(
-                                        onTap: (){
-                                          Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context){
-                                                    return   newclass(snapshot.data["definitions"][index]["image_url"],_controller.text);
-                                                  }
-                                              )
-                                          );
-                                        },
-                                        child: CircleAvatar(
-                                          backgroundColor: Colors.pinkAccent,
-                                          backgroundImage: NetworkImage(snapshot.data["definitions"][index]["image_url"]),
-                                          radius: 30,
-                                        ),
-                                      )
-                                  ),
-                                ),
-                                title:snapshot.data["definitions"][index]["type"]==null ?null : Text(_controller.text.trim().toString()+"{"+snapshot.data["definitions"][index]["type"]+"}",style: TextStyle(color: Colors.white,fontSize: 22,fontWeight: FontWeight.bold),),
-
+                                leading: snapshot.data["definitions"][index]
+                                            ["image_url"] ==
+                                        null
+                                    ? null
+                                    : Hero(
+                                        tag: "$val",
+                                        child: Container(
+                                            child: GestureDetector(
+                                          onTap: () {
+                                            Navigator.push(context,
+                                                MaterialPageRoute(
+                                                    builder: (context) {
+                                              return newclass(
+                                                  snapshot.data["definitions"]
+                                                      [index]["image_url"],
+                                                  _controller.text);
+                                            }));
+                                          },
+                                          child: CircleAvatar(
+                                            backgroundColor: Colors.pinkAccent,
+                                            backgroundImage: NetworkImage(
+                                                snapshot.data["definitions"]
+                                                    [index]["image_url"]),
+                                            radius: 30,
+                                          ),
+                                        )),
+                                      ),
+                                title: snapshot.data["definitions"][index]
+                                            ["type"] ==
+                                        null
+                                    ? null
+                                    : Text(
+                                        _controller.text.trim().toString() +
+                                            "{" +
+                                            snapshot.data["definitions"][index]
+                                                ["type"] +
+                                            "}",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold),
+                                      ),
                               ),
                             ),
                             Padding(
                                 padding: const EdgeInsets.all(8.0),
-                                child:Container(
+                                child: Container(
                                   color: Colors.pinkAccent,
-                                  child: snapshot.data["definitions"][index]["definition"]==null ?null : Text("${snapshot.data["definitions"][index]["definition"]}",style: TextStyle(color: Colors.white,fontSize: 20),),
-                                )
-                            ),
+                                  child: snapshot.data["definitions"][index]
+                                              ["definition"] ==
+                                          null
+                                      ? null
+                                      : Text(
+                                          "${snapshot.data["definitions"][index]["definition"]}",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ),
+                                )),
                           ],
                         ),
                       ),
@@ -244,5 +270,4 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
       ),
     );
   }
-
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -6,33 +6,29 @@ import 'package:dictionary/MyApp.dart';
 import 'package:flutter/material.dart';
 
 class splashScreen extends StatefulWidget {
-
   @override
   _splashScreenState createState() => _splashScreenState();
 }
 
-class _splashScreenState extends State<splashScreen> with SingleTickerProviderStateMixin{
+class _splashScreenState extends State<splashScreen>
+    with SingleTickerProviderStateMixin {
   var spinkit;
 
   @override
   void initState() {
     super.initState();
-     spinkit = SpinKitCircle(
+    spinkit = SpinKitCircle(
       color: Colors.pink,
       size: 170.0,
-     );
-    Timer(
-      Duration(seconds: 3),(){
-        Navigator.of(context).pop();
-        Navigator.of(context).push(MaterialPageRoute(
-          builder: (context){
-
-            return MyApp();
-          }
-        ));
-    }
     );
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pop();
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return MyApp();
+      }));
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,41 +37,39 @@ class _splashScreenState extends State<splashScreen> with SingleTickerProviderSt
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.pink,Colors.green]
-            )
-          ),
+              gradient: LinearGradient(List: [Colors.pink, Colors.green])),
           child: Stack(
             fit: StackFit.loose,
             children: <Widget>[
               Positioned(
-                left: 120,
-                top: 150,
-                child: Container(
-                  padding: EdgeInsets.all(16.0),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    gradient: LinearGradient(
-                      colors: [Colors.green[200],Colors.pinkAccent[200]]
-                    )
-                  ),
-                  child: Image.asset("asset/dictionary.png"),
-                )
+                  left: 120,
+                  top: 150,
+                  child: Container(
+                    padding: EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        gradient: LinearGradient(
+                            List: [Colors.green[200], Colors.pinkAccent[200]])),
+                    child: Image.asset("asset/dictionary.png"),
+                  )),
+              Positioned(
+                child: spinkit,
+                top: 130,
+                left: 100,
               ),
-             Positioned(
-               child:  spinkit,
-               top: 130,
-               left: 100,
-             ),
               Container(
-                margin: EdgeInsets.only(left: 80,top: 360),
-                child: Text("Design By Shahmeer",style: TextStyle(fontWeight: FontWeight.normal,color: Colors.white70,fontSize: 22),)
-              )
+                  margin: EdgeInsets.only(left: 80, top: 360),
+                  child: Text(
+                    "Design By Shahmeer",
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white70,
+                        fontSize: 22),
+                  ))
             ],
           ),
         ),
       ),
     );
   }
-
 }
